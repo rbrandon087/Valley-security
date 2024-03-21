f85ef579c0904203ae5dca0dda4d39bbc518e2e1
Brandon <rbrandon087@gmail.com>
2024-03-20 21:41:10 -0700

    added a phone number section to the careers page

--- client/src/pages/Careers.tsx
+++ client/src/pages/Careers.tsx
@@ -46,6 +46,23 @@ const Careers: React.FunctionComponent<ICareersProps> = ({ name }) => {
                 />
               </div>
             </div>
+            <div className="sm:col-span-4">
+              <label
+                htmlFor="phone"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Phone
+              </label>
+              <div className="mt-2">
+                <input
+                  type="tel"
+                  name="phone"
+                  id="phone"
+                  autoComplete="tel"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
 
             <div className="sm:col-span-4">
               <label

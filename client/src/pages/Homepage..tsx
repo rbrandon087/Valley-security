05b19adce649a3ed9d9efea5194c217330a23dce
Brandon <rbrandon087@gmail.com>
2024-03-25 22:01:11 -0700

    added more images to homepage and changed the color gradient at the top of the homepage

--- client/src/pages/Homepage..tsx
+++ client/src/pages/Homepage..tsx
@@ -1,5 +1,10 @@
 import React from "react";
 import Skyarc from "../public/Sky-arc.jpg.jpg";
+import Bath from "../public/bathroom.jpg.jpg";
+import Close from "../public/closeup.jpg";
+import Floor from "../public/floor-view.jpg";
+import Shiny from "../public/white.jpg";
+import Bedroom from "../public/window-view.jpg";
 
 export interface IHomepageProps {
   name: string;
@@ -46,7 +51,7 @@ const Homepage: React.FunctionComponent<IHomepageProps> = ({ name }) => {
           aria-hidden="true"
         >
           <div
-            className="aspect-[801/1036] w-[50.0625rem] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30"
+            className="aspect-[801/1036] w-[50.0625rem] bg-gradient-to-tr from-state to-light opacity-30"
             style={{
               clipPath:
                 "polygon(63.1% 29.5%, 100% 17.1%, 76.6% 3%, 48.4% 0%, 44.6% 4.7%, 54.5% 25.3%, 59.8% 49%, 55.2% 57.8%, 44.4% 57.2%, 27.8% 47.9%, 35.1% 81.5%, 0% 97.7%, 39.2% 100%, 35.2% 81.4%, 97.2% 52.8%, 63.1% 29.5%)",
@@ -78,8 +83,8 @@ const Homepage: React.FunctionComponent<IHomepageProps> = ({ name }) => {
                 <div className="ml-auto w-44 flex-none space-y-8 pt-32 sm:ml-0 sm:pt-80 lg:order-last lg:pt-36 xl:order-none xl:pt-80">
                   <div className="relative">
                     <img
-                      src="https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&h=528&q=80"
-                      alt=""
+                      src={Bath}
+                      alt="Bathroom floor image"
                       className="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
                     />
                     <div className="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
@@ -88,7 +93,7 @@ const Homepage: React.FunctionComponent<IHomepageProps> = ({ name }) => {
                 <div className="mr-auto w-44 flex-none space-y-8 sm:mr-0 sm:pt-52 lg:pt-36">
                   <div className="relative">
                     <img
-                      src="https://images.unsplash.com/photo-1485217988980-11786ced9454?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&h=528&q=80"
+                      src={Close}
                       alt=""
                       className="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
                     />
@@ -96,7 +101,7 @@ const Homepage: React.FunctionComponent<IHomepageProps> = ({ name }) => {
                   </div>
                   <div className="relative">
                     <img
-                      src="https://images.unsplash.com/photo-1559136555-9303baea8ebd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-x=.4&w=396&h=528&q=80"
+                      src={Floor}
                       alt=""
                       className="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
                     />
@@ -106,7 +111,7 @@ const Homepage: React.FunctionComponent<IHomepageProps> = ({ name }) => {
                 <div className="w-44 flex-none space-y-8 pt-32 sm:pt-0">
                   <div className="relative">
                     <img
-                      src="https://images.unsplash.com/photo-1670272504528-790c24957dda?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=left&w=400&h=528&q=80"
+                      src={Shiny}
                       alt=""
                       className="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
                     />
@@ -114,7 +119,7 @@ const Homepage: React.FunctionComponent<IHomepageProps> = ({ name }) => {
                   </div>
                   <div className="relative">
                     <img
-                      src="https://images.unsplash.com/photo-1670272505284-8faba1c31f7d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&h=528&q=80"
+                      src={Bedroom}
                       alt=""
                       className="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
                     />
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,6 +5,8 @@ module.exports = {
     extend: {
       colors: {
         'bar': '#2E4F88',
+        'state': '#1127ba',
+        'light': '#81d3ef',
       },
       spacing: {
         '112': '52rem', // Add your custom margin value here

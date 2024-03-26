bd60999840e3de8bc708d7e049b24af1b56eaf29
Brandon <rbrandon087@gmail.com>
2024-03-25 22:50:24 -0700

    took off the fixed

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -15,8 +15,8 @@ const Navbar: React.FC<INavbarProps> = ({ name }) => {
   };
 
   return (
-    <div className="flex justify-center">
-      <nav className="fixed top-0 z-50">
+    <div>
+      <nav className="">
         <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
           <div className="relative flex h-16 items-center justify-between">
             <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">

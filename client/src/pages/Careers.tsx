f550069ee5a9bea45ffc372850ccaea5efef4ac9
Brandon <rbrandon087@gmail.com>
2024-03-28 23:49:39 -0700

    check

--- client/src/pages/Careers.tsx
+++ client/src/pages/Careers.tsx
@@ -15,7 +15,7 @@ const Careers: React.FunctionComponent<ICareersProps> = ({ name }) => {
           alt="Floor Installation"
           className="bg-cover bg-center h-96 w-full bg-opacity-50"
         />
-        <div className="mx-auto max-w-xl lg:mx-0 lg:max-w-lg place-items-center">
+        <div className="mx-auto max-w-xl lg:mx-0 lg:max-w-lg place-items-center ">
           <h2 className="absolute mt-24 top-0 left-0 right-0 bottom-0 flex items-center justify-center text-5xl font-bold tracking-tight text-white">
             Join Our Team
           </h2>
@@ -39,7 +39,7 @@ const Careers: React.FunctionComponent<ICareersProps> = ({ name }) => {
                       name="first-name"
                       id="first-name"
                       autoComplete="given-name"
-                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-bar sm:text-sm sm:leading-6"
                     />
                   </div>
                 </div>

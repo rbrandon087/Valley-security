f01580d192ee15aa6d6bc9439b92cda649a02346
Brandon <rbrandon087@gmail.com>
2024-04-01 22:17:30 -0700

    added a new compoenet to reviewsection

--- client/src/components/ReviewSection.tsx
+++ client/src/components/ReviewSection.tsx
@@ -9,18 +9,22 @@ const ReviewSection: React.FunctionComponent<IReviewSectionProps> = ({
   name,
 }) => {
   return (
-    <div>
-      {/* Ratings bar */}
-      <div className="min-h-0.5 mt-40">
-        <footer className="bg-grey px-2 sm:px-6 lg:px-8">
-          <div className="mx-auto max-w-7xl px-6 py-12 sm:flex md:items-center sm:justify-center lg:px-96 ">
-            <div className="mt-8 md:order-1 md:mt-0">
-              <h1 className="text-center text-3xl font-bold tracking-tight sm:text-3xl text-gray-200">
-                Reviews
-              </h1>
-            </div>
-          </div>
-        </footer>
+    <div className="bg-indigo-100 mt-24">
+      <div className="mx-auto max-w-7xl px-6 py-24 sm:py-32 lg:flex lg:items-center lg:justify-between lg:px-8">
+        <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+          See what people have to say about us.
+        </h2>
+        <div className="mt-10 flex items-center gap-x-6 lg:mt-0 lg:flex-shrink-0">
+          <a
+            href="#"
+            className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            Get started
+          </a>
+          <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+            Learn more <span aria-hidden="true">→</span>
+          </a>
+        </div>
       </div>
     </div>
   );

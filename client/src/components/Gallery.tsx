276cb4d088224a296386597293aaa5b2499a6584
Brandon <rbrandon087@gmail.com>
2024-04-01 22:50:26 -0700

    coming up with some ideas what i can do in gallery

--- client/src/components/Gallery.tsx
+++ client/src/components/Gallery.tsx
@@ -16,48 +16,6 @@ const Gallery: React.FunctionComponent<IGalleryProps> = ({ name }) => {
             Learn how to grow your business with our expert advice.
           </p>
         </div>
-        <div className="mx-auto mt-24 grid max-w-2xl auto-rows-fr grid-cols-1 gap-8 sm:mt-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-          <article className="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl bg-gray-900 px-8 pb-8 pt-80 sm:pt-48 lg:pt-80">
-            <img
-              src=""
-              alt=""
-              className="absolute inset-0 -z-10 h-full w-full object-cover"
-            />
-            <div className="absolute inset-0 -z-10 bg-gradient-to-t from-gray-900 via-gray-900/40" />
-            <div className="absolute inset-0 -z-10 rounded-2xl ring-1 ring-inset ring-gray-900/10" />
-
-            <div className="flex flex-wrap items-center gap-y-1 overflow-hidden text-sm leading-6 text-gray-300">
-              <div className="-ml-4 flex items-center gap-x-4">
-                <svg
-                  viewBox="0 0 2 2"
-                  className="-ml-0.5 h-0.5 w-0.5 flex-none fill-white/50"
-                >
-                  <circle cx={1} cy={1} r={1} />
-                </svg>
-              </div>
-            </div>
-          </article>
-          <article className="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl bg-gray-900 px-8 pb-8 pt-80 sm:pt-48 lg:pt-80">
-            <img
-              src=""
-              alt=""
-              className="absolute inset-0 -z-10 h-full w-full object-cover"
-            />
-            <div className="absolute inset-0 -z-10 bg-gradient-to-t from-gray-900 via-gray-900/40" />
-            <div className="absolute inset-0 -z-10 rounded-2xl ring-1 ring-inset ring-gray-900/10" />
-
-            <div className="flex flex-wrap items-center gap-y-1 overflow-hidden text-sm leading-6 text-gray-300">
-              <div className="-ml-4 flex items-center gap-x-4">
-                <svg
-                  viewBox="0 0 2 2"
-                  className="-ml-0.5 h-0.5 w-0.5 flex-none fill-white/50"
-                >
-                  <circle cx={1} cy={1} r={1} />
-                </svg>
-              </div>
-            </div>
-          </article>
-        </div>
       </div>
     </div>
   );

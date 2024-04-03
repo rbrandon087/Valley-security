0de537ff775e8513990b0c0ce65f3ff891e8434e
Brandon <rbrandon087@gmail.com>
2024-04-02 22:40:03 -0700

    ill stop here pick up tomorrow

--- client/src/pages/Residential.tsx
+++ client/src/pages/Residential.tsx
@@ -58,13 +58,6 @@ const Residential: React.FunctionComponent<IResidentialProps> = ({ name }) => {
                   className="h-full w-full object-cover object-center group-hover:opacity-75"
                 />
               </div>
-              <div className="mt-4 flex items-center justify-between text-base font-medium text-gray-900">
-                <h3>{product.name}</h3>
-                <p>{product.price}</p>
-              </div>
-              <p className="mt-1 text-sm italic text-gray-500">
-                {product.description}
-              </p>
             </a>
           ))}
         </div>

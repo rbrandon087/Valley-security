eabb49b5ee52be22dab3e461398d8bcc8e9bf327
Brandon <rbrandon087@gmail.com>
2024-04-06 13:00:22 -0700

    resized my image for gallery and adjusted the hover translate to 65

--- client/src/components/Gallery.tsx
+++ client/src/components/Gallery.tsx
@@ -8,6 +8,7 @@ export interface IGalleryProps {
 const Gallery: React.FunctionComponent<IGalleryProps> = ({ name }) => {
   return (
     <div className="bg-white py-24 sm:py-32 ">
+      {/* Intro Title*/}
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl text-center">
           <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
@@ -22,7 +23,7 @@ const Gallery: React.FunctionComponent<IGalleryProps> = ({ name }) => {
       <div className="flex min-h-screen items-center justify-center  ">
         <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-2  ">
           <div className="group relative cursor-pointer items-center justify-center  overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30">
-            <div className="h-96 w-72 mx-auto ">
+            <div className="h-120 w-96 mx-auto ">
               <img
                 className="h-full w-full object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125 "
                 src={Display}
@@ -30,13 +31,14 @@ const Gallery: React.FunctionComponent<IGalleryProps> = ({ name }) => {
               />
             </div>
             <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
-            <div className="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
+            <div className="absolute inset-0 flex translate-y-[65%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
               <h1 className="font-dmserif text-3xl font-bold text-white">
                 Residential
               </h1>
               <p className="mb-3 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
-                dolore adipisci placeat.
+                Step inside and discover the beauty that lies beneath your feet.
+                Our expert team is here to transform your space, one floor at a
+                time..
               </p>
               <a href="/Residential">
                 <button className="rounded-full bg-neutral-900 py-2 px-3.5 font-com text-sm capitalize text-white shadow shadow-black/60">
@@ -46,7 +48,7 @@ const Gallery: React.FunctionComponent<IGalleryProps> = ({ name }) => {
             </div>
           </div>
           <div className="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30">
-            <div className="h-96 w-72 mx-auto">
+            <div className="h-full w-96 mx-auto">
               <img
                 className="h-full w-full object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125"
                 src={Dance}
@@ -55,13 +57,15 @@ const Gallery: React.FunctionComponent<IGalleryProps> = ({ name }) => {
             </div>
             {/* first image*/}
             <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
-            <div className="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
+            <div className="absolute inset-0 flex translate-y-[65%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
               <h1 className="font-dmserif text-3xl font-bold text-white">
                 Commercial
               </h1>
               <p className="mb-3 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
-                dolore adipisci placeat.
+                Step into a world of possibilities. Our flooring solutions are
+                designed to elevate your business space, leaving a lasting
+                impression on clients and customers alike. Let’s create
+                something remarkable together!
               </p>
               <a href="./Commercial">
                 <button className="rounded-full bg-neutral-900 py-2 px-3.5 font-com text-sm capitalize text-white shadow shadow-black/60">

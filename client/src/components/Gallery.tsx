802168866510ce48f3049b6b7ceacdf8e2dd28c0
Brandon <rbrandon087@gmail.com>
2024-04-07 17:10:06 -0700

    adjusting the gallery for mobile view

--- client/src/components/Gallery.tsx
+++ client/src/components/Gallery.tsx
@@ -25,7 +25,7 @@ const Gallery: React.FunctionComponent<IGalleryProps> = ({ name }) => {
       <div className="flex min-h-screen items-center justify-center  ">
         <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-2  ">
           <div className="group relative cursor-pointer items-center justify-center  overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30">
-            <div className="h-96 w-96 mx-auto  ">
+            <div className="h-88 w-96 mx-auto  ">
               <LazyLoad>
                 <img
                   className="h-full w-full object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125 "
@@ -35,7 +35,7 @@ const Gallery: React.FunctionComponent<IGalleryProps> = ({ name }) => {
               </LazyLoad>
             </div>
             <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
-            <div className="absolute inset-0 flex translate-y-[65%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
+            <div className="absolute inset-0 flex translate-y-[75%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
               <h1 className="font-dmserif text-3xl font-bold text-white">
                 Residential
               </h1>
@@ -52,7 +52,7 @@ const Gallery: React.FunctionComponent<IGalleryProps> = ({ name }) => {
             </div>
           </div>
           <div className="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30">
-            <div className="h-fulls w-96 ob mx-auto">
+            <div className="h-88 w-96  mx-auto">
               <LazyLoad>
                 <img
                   className="h-full w-full object-contain transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125"

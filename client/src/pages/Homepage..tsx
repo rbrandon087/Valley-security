f893bdb6254ece49bf6be80999945b3b89616c25
Brandon <rbrandon087@gmail.com>
2024-04-30 22:36:16 -0700

    removed more of a review section

--- client/src/pages/Homepage..tsx
+++ client/src/pages/Homepage..tsx
@@ -184,63 +184,6 @@ const Homepage: React.FunctionComponent<IHomepageProps> = ({ name }) => {
       <main />
       {/* Demolation Section */}
       <Demo name={""} />
-
-      {/* Ratings Section */}
-      <div className="bg-white grid h-screen place-items-center ">
-        <div>
-          <h2 id="reviews-heading" className="sr-only">
-            Reviews
-          </h2>
-
-          <div className="space-y-4 -mt-44">
-            {" "}
-            {/* Adjusted space between review blocks */}
-            {reviews.map((review) => (
-              <div key={review.id} className="flex flex-col sm:flex-row">
-                <div className="order-2 mt-6 sm:ml-14 sm:mt-0">
-                  <h3 className="text-sm font-medium text-gray-900">
-                    {review.title}
-                  </h3>
-                  <p className="sr-only">{review.rating} out of 5 stars</p>
-
-                  <div
-                    className="mt-1 space-y-2 text-sm text-gray-600"
-                    dangerouslySetInnerHTML={{ __html: review.content }}
-                  />
-                </div>
-
-                <div className="order-1 flex items-center sm:flex-col sm:items-start">
-                  <img
-                    src={review.avatarSrc}
-                    alt={`${review.author}.`}
-                    className="h-12 w-12 rounded-full"
-                  />
-
-                  <div className="ml-4 sm:ml-0 sm:mt-4">
-                    <p className="text-sm font-medium text-gray-900">
-                      {review.author}
-                    </p>
-                    <div className="mt-2 flex items-center">
-                      {[0, 1, 2, 3, 4].map((rating) => (
-                        <StarIcon
-                          key={rating}
-                          className={classNames(
-                            review.rating > rating
-                              ? "text-gray-900"
-                              : "text-gray-200",
-                            "h-5 w-5 flex-shrink-0",
-                          )}
-                          aria-hidden="true"
-                        />
-                      ))}
-                    </div>
-                  </div>
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
-      </div>
     </div>
   );
 };

48a063f39fa42d2fd097f754a9ea6315be9a11bf
Brandon <rbrandon087@gmail.com>
2024-05-12 23:23:35 -0700

    working on a image on home

--- client/src/pages/Homepage..tsx
+++ client/src/pages/Homepage..tsx
@@ -287,15 +287,7 @@ const Homepage: React.FunctionComponent<IHomepageProps> = ({ name }) => {
             </div>
           </div>
 
-          <div className="mt-6 px-4 sm:hidden">
-            <a
-              href="#"
-              className="block text-sm font-semibold text-indigo-600 hover:text-indigo-500"
-            >
-              Browse all categories
-              <span aria-hidden="true"> &rarr;</span>
-            </a>
-          </div>
+          <div className="mt-6 px-4 sm:hidden"></div>
 
           {/* Featured section */}
           <section
@@ -307,30 +299,17 @@ const Homepage: React.FunctionComponent<IHomepageProps> = ({ name }) => {
                 <img
                   src="https://tailwindui.com/img/ecommerce-images/home-page-01-feature-section-01.jpg"
                   alt=""
-                  className="h-full w-full object-cover object-center"
+                  className="h-40 md:h-72 w-full object-cover"
                 />
               </div>
               <div className="relative bg-gray-900 bg-opacity-75 px-6 py-32 sm:px-12 sm:py-40 lg:px-16">
                 <div className="relative mx-auto flex max-w-3xl flex-col items-center text-center">
-                  <h2
-                    id="social-impact-heading"
-                    className="text-3xl font-bold tracking-tight text-white sm:text-4xl"
-                  >
-                    <span className="block sm:inline">Level up</span>
-                    <span className="block sm:inline">your desk</span>
-                  </h2>
-                  <p className="mt-3 text-xl text-white">
+                  <p className="mt-0 text-xl text-white">
                     Make your desk beautiful and organized. Post a picture to
                     social media and watch it get more likes than life-changing
                     announcements. Reflect on the shallow nature of existence.
                     At least you have a really nice desk setup.
                   </p>
-                  <a
-                    href="#"
-                    className="mt-8 block w-full rounded-md border border-transparent bg-white px-8 py-3 text-base font-medium text-gray-900 hover:bg-gray-100 sm:w-auto"
-                  >
-                    Shop Workspace
-                  </a>
                 </div>
               </div>
             </div>

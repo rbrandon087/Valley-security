6d1616fa2a9cb1534bd9035c37572c198dc66cb2
Brandon <rbrandon087@gmail.com>
2024-05-16 22:54:21 -0700

    adjusted more in my form using padding and margin and with max w lg puting my form in a container with a shadow as well

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -40,9 +40,6 @@ const Footer: React.FunctionComponent<IFooterProps> = ({ name }) => {
   return (
     <div className="min-h-0.5">
       <div className="relative">
-        <div className="absolute inset-0 flex items-center" aria-hidden="true">
-          <div className="w-full border-t border-red-900" />
-        </div>
         <div className="relative flex justify-center"></div>
       </div>
       <footer className="bg-gray-900 px-2 sm:px-6 lg:px-8">
--- client/src/pages/Careers.tsx
+++ client/src/pages/Careers.tsx
@@ -60,177 +60,178 @@ const Careers: React.FunctionComponent<ICareersProps> = ({ name }) => {
   };
 
   return (
-    <div className="relative bg-gray-900 mt-0 flex justify-center items-center h-screen">
-      <div className="">
+    <div className="relative bg-gray-900 mt-0 flex justify-center items-center min-h-screen px-4 sm:px-6 lg:px-8">
+      <div className="w-full max-w-xl lg:max-w-lg">
         {/* Form submit */}
-
         <form
           onSubmit={handleSubmit(onSubmit)}
           action="/api/careers"
           method="POST"
-          className="mt-16"
+          className="mt-8 bg-grey-900 p-6 rounded-lg shadow-md"
         >
-          <div className="mx-auto max-w-xl lg:mr-0 lg:max-w-lg">
-            <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
-              <div>
-                <label
-                  htmlFor="first-name"
-                  className="block text-sm font-semibold leading-6 text-white"
-                >
-                  First name
-                </label>
-                <div className="mt-2.5">
-                  <input
-                    {...register("firstName", {
-                      required: "First name is required",
-                    })}
-                    type="text"
-                    name="firstName"
-                    id="first-name"
-                    autoComplete="name"
-                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  />
-                  {errors.firstName && (
-                    <div className="text-red-500">
-                      {errors.firstName.message}
-                    </div>
-                  )}
-                </div>
-              </div>
-              <div>
-                <label
-                  htmlFor="last-name"
-                  className="block text-sm font-semibold leading-6 text-white"
-                >
-                  Last name
-                </label>
-                <div className="mt-2.5">
-                  <input
-                    {...register("lastName", {
-                      required: "Last name is required",
-                    })}
-                    type="text"
-                    name="lastName"
-                    id="last-name"
-                    autoComplete="family-name"
-                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  />
-                  {errors.lastName && (
-                    <div className="text-red-500">
-                      {errors.lastName.message}
-                    </div>
-                  )}
-                </div>
-              </div>
-              <div className="sm:col-span-2">
-                <label
-                  htmlFor="email"
-                  className="block text-sm font-semibold leading-6 text-white"
-                >
-                  Email
-                </label>
-                <div className="mt-2.5">
-                  <input
-                    {...register("email", {
-                      required: "Email is required",
-                      validate: (value) => {
-                        if (!value.includes("@")) {
-                          return "Email is required";
-                        }
-                        return true;
-                      },
-                    })}
-                    type="email"
-                    name="email"
-                    id="email"
-                    autoComplete="email"
-                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  />
-                  {errors.email && (
-                    <div className="text-red-500">{errors.email.message}</div>
-                  )}
-                </div>
-              </div>
-              <div className="sm:col-span-2">
-                <label
-                  htmlFor="phone-number"
-                  className="block text-sm font-semibold leading-6 text-white"
-                >
-                  Phone number
-                </label>
-                <div className="mt-2.5">
-                  <input
-                    {...register("phone_number", {
-                      required: "Phone number is required",
-                    })}
-                    type="tel"
-                    name="phone_number"
-                    id="phone-number"
-                    autoComplete="tel"
-                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  />
-                  {errors.phone_number && (
-                    <div className="text-red-500">
-                      {errors.phone_number.message}
-                    </div>
-                  )}
-                </div>
-              </div>
-              {/* Resume */}
-              <div className="mt-2 flex justify-center rounded-lg border bg-white px-6 py-10 sm:col-span-2">
-                <div className="text-center text-white">
-                  <div className="mt-4 flex text-sm leading-6 text-gray-400">
-                    <label
-                      htmlFor="file-upload"
-                      className="relative cursor-pointer rounded-md bg-transparent border font-semibold text-white focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 focus-within:ring-offset-gray-900 hover:text-bar"
-                    >
-                      <span className="text-black">Upload a file</span>
-                      <input
-                        {...register("resume", { required: "Resume needed" })}
-                        id="file-upload"
-                        name="resumeFile"
-                        type="file"
-                        className="sr-only inset-0 w-full h-full opacity-0 cursor-pointer "
-                      />
-                    </label>
-                    <p className="pl-1 text-black">or drag and drop</p>
-                  </div>
-                  <p className="text-xs leading-5 text-black">
-                    PNG, JPG, GIF up to 10MB
-                  </p>
-                  {errors.resume && (
-                    <div className="text-red-500">{errors.resume.message}</div>
-                  )}
-                </div>
-              </div>
-              {/* Message form */}
-              <div className="sm:col-span-2">
-                <label
-                  htmlFor="message"
-                  className="block text-sm font-semibold leading-6 text-white"
-                >
-                  Message
-                </label>
-                <div className="mt-2.5">
-                  <textarea
-                    {...register("message")}
-                    name="message"
-                    id="message"
-                    rows={4}
-                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    defaultValue={""}
-                  />
-                </div>
-              </div>
-            </div>
-            <div className="mt-8 flex justify-end">
-              <button
-                type="submit"
-                className="bg-bar hover:bg-grey text-white font-bold py-2 px-4 rounded-full"
+          <div className="grid grid-cols-1 gap-6 sm:grid-cols-2">
+            <div>
+              <label
+                htmlFor="first-name"
+                className="block text-sm font-semibold leading-6 text-white"
               >
-                Submit
-              </button>
+                First name
+              </label>
+              <div className="mt-2.5">
+                <input
+                  {...register("firstName", {
+                    required: "First name is required",
+                  })}
+                  type="text"
+                  name="firstName"
+                  id="first-name"
+                  autoComplete="name"
+                  className="block w-full rounded-md border px-3.5 py-2 text-gray-900 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm sm:leading-6"
+                />
+                {errors.firstName && (
+                  <div className="text-red-500 text-sm mt-1">
+                    {errors.firstName.message}
+                  </div>
+                )}
+              </div>
             </div>
+            <div>
+              <label
+                htmlFor="last-name"
+                className="block text-sm font-semibold leading-6 text-white"
+              >
+                Last name
+              </label>
+              <div className="mt-2.5">
+                <input
+                  {...register("lastName", {
+                    required: "Last name is required",
+                  })}
+                  type="text"
+                  name="lastName"
+                  id="last-name"
+                  autoComplete="family-name"
+                  className="block w-full rounded-md border px-3.5 py-2 text-gray-900 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm sm:leading-6"
+                />
+                {errors.lastName && (
+                  <div className="text-red-500 text-sm mt-1">
+                    {errors.lastName.message}
+                  </div>
+                )}
+              </div>
+            </div>
+            <div className="sm:col-span-2">
+              <label
+                htmlFor="email"
+                className="block text-sm font-semibold leading-6 text-white"
+              >
+                Email
+              </label>
+              <div className="mt-2.5">
+                <input
+                  {...register("email", {
+                    required: "Email is required",
+                    validate: (value) => {
+                      if (!value.includes("@")) {
+                        return "Email is required";
+                      }
+                      return true;
+                    },
+                  })}
+                  type="email"
+                  name="email"
+                  id="email"
+                  autoComplete="email"
+                  className="block w-full rounded-md border px-3.5 py-2 text-gray-900 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm sm:leading-6"
+                />
+                {errors.email && (
+                  <div className="text-red-500 text-sm mt-1">
+                    {errors.email.message}
+                  </div>
+                )}
+              </div>
+            </div>
+            <div className="sm:col-span-2">
+              <label
+                htmlFor="phone-number"
+                className="block text-sm font-semibold leading-6 text-white"
+              >
+                Phone number
+              </label>
+              <div className="mt-2.5">
+                <input
+                  {...register("phone_number", {
+                    required: "Phone number is required",
+                  })}
+                  type="tel"
+                  name="phone_number"
+                  id="phone-number"
+                  autoComplete="tel"
+                  className="block w-full rounded-md border px-3.5 py-2 text-gray-900 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm sm:leading-6"
+                />
+                {errors.phone_number && (
+                  <div className="text-red-500 text-sm mt-1">
+                    {errors.phone_number.message}
+                  </div>
+                )}
+              </div>
+            </div>
+            {/* Resume */}
+            <div className="mt-2 flex justify-center items-center rounded-lg border bg-gray-100 px-6 py-10 sm:col-span-2">
+              <div className="text-center">
+                <div className="mt-4 flex text-sm leading-6 text-gray-400">
+                  <label
+                    htmlFor="file-upload"
+                    className="relative cursor-pointer rounded-md bg-white border font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:text-indigo-500"
+                  >
+                    <span>Upload a file</span>
+                    <input
+                      {...register("resume", { required: "Resume needed" })}
+                      id="file-upload"
+                      name="resumeFile"
+                      type="file"
+                      className="sr-only"
+                    />
+                  </label>
+                  <p className="pl-1">or drag and drop</p>
+                </div>
+                <p className="text-xs leading-5 text-gray-500">
+                  PNG, JPG, GIF up to 10MB
+                </p>
+                {errors.resume && (
+                  <div className="text-red-500 text-sm mt-1">
+                    {errors.resume.message}
+                  </div>
+                )}
+              </div>
+            </div>
+            {/* Message form */}
+            <div className="sm:col-span-2">
+              <label
+                htmlFor="message"
+                className="block text-sm font-semibold leading-6 text-white"
+              >
+                Message
+              </label>
+              <div className="mt-2.5">
+                <textarea
+                  {...register("message")}
+                  name="message"
+                  id="message"
+                  rows={4}
+                  className="block w-full rounded-md border px-3.5 py-2 text-gray-900 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm sm:leading-6"
+                  defaultValue={""}
+                />
+              </div>
+            </div>
+          </div>
+          <div className="mt-10 flex justify-end border-t border-gray-200 pt-8">
+            <button
+              type="submit"
+              className="bg-indigo-600 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded-full"
+            >
+              Submit
+            </button>
           </div>
         </form>
       </div>
